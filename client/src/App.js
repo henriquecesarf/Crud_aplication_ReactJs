9d11aac73c84ad671df970054a0f585312608ae1
Henrique Dantas <henriquecfdantas@gmail.com>
2022-01-20 22:50:09 -0300

    ajustes style

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Card from "./components/cards/Card";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave } from '@fortawesome/free-solid-svg-icons'
+
+
 function App() {
     const [values, setValues] = useState();
     const [listfunc, setlistfunc] = useState();
@@ -39,63 +43,66 @@ function App() {
 
     };
 
-    return ( <
-        div className = "app-container" >
-        <
-        div className = "register-container" >
-        <
-        h1 className = "register-title" > CRUD APPLICATION < /h1>
+    return ( 
+        
+        <div className = "app-container" >
+            
+            <div className = "register-container">
+                <h1 className = "register-title"> CRUD APPLICATION </h1>
 
-        <
-        input type = "text"
-        name = "name"
-        placeholder = "Nome"
-        className = "register-input"
-        onChange = { handleaddValues }
-        /> <
-        input type = "text"
-        placeholder = "Email"
-        name = "email"
-        className = "register-input"
-        onChange = { handleaddValues }
-        /> <
-        input type = "text"
-        placeholder = "Número"
-        name = "number"
-        className = "register-input"
-        onChange = { handleaddValues }
-        />
+                <input 
+                    type = "text"
+                    name = "name"
+                    placeholder = "Nome"
+                    className = "register-input"
+                    onChange = { handleaddValues }
+                /> 
+                <input 
+                    type = "text"
+                    placeholder = "Email"
+                    name = "email"
+                    className = "register-input"
+                    onChange = { handleaddValues }
+                /> 
+                <input 
+                    type = "text"
+                    placeholder = "Número"
+                    name = "number"
+                    className = "register-input"
+                    onChange = { handleaddValues }
+                />
 
-        <
-        button onClick = { handleClickButton }
-        className = "register-button" >
-        Cadastrar <
-        /button> <
-        /div>
-
-
-        {
-            typeof listfunc !== "undefined" && listfunc.map((value) => {
-                return ( <
-                    Card key = { value.id }
-                    listfunc = { listfunc }
-                    setlistfunc = { setlistfunc }
-                    id = { value.id }
-                    name = { value.funcNome }
-                    email = { value.funcEmail }
-                    number = { value.funciNumero }
-
-                    >
-
-                    <
-                    /Card>
-                );
-            })
-        }
+                <button 
+                    onClick = { handleClickButton }
+                    className = "register-button" 
+                >
+                    Cadastrar 
+                    <FontAwesomeIcon icon={faSave} size="1x" />
+                </button> 
+            </div>
 
 
-        <
-        /div>
+            {
+                typeof listfunc !== "undefined" && listfunc.map((value) => {
+                    return ( 
+                        <Card 
+                            key = { value.id }
+                            listfunc = { listfunc }
+                            setlistfunc = { setlistfunc }
+                            id = { value.id }
+                            name = { value.funcNome }
+                            email = { value.funcEmail }
+                            number = { value.funciNumero }
+
+                        >
+
+                        </Card>
+                    );
+                })
+            }
+
+
+        </div>
     );
 }
 
