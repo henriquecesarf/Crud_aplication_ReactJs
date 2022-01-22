00323a61b457b10c672759c6c865e5e9189672b4
Henrique Dantas <henriquecfdantas@gmail.com>
2022-01-22 18:04:54 -0300

    autenticação de login

--- client/src/Routers.js
+++ client/src/Routers.js
@@ -3,7 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./Home";
 import Registro from "./Registro";
-
+import Cadastro from "./Cadastro";
 
 const Routers = () => {
     return (
@@ -11,7 +11,7 @@ const Routers = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="Registro" element={<Registro />} />
-             
+                <Route path="Cadastro" element={<Cadastro />} />
             </Routes>
         </BrowserRouter>
     )
